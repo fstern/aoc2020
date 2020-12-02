e40cf32691b77c7de8e67a5b2a64ded535a79132
Falk Stern <falk@fourecks.de>
2020-12-02 23:44:10 +0100

    removed comment

--- day2_2.fish
+++ day2_2.fish
@@ -5,7 +5,6 @@ while read num letter password ;
     set one ( echo $num|cut -d- -f1 )
     set two ( echo $num|cut -d- -f2 )
     set letter ( echo $letter | sed s/:// )
-    #echo $letter $one $two $password
     echo $password | fold -w 1 | head -n $one | tail -1 | grep $letter >/dev/null ; set r1 $status ;
     echo $password | fold -w 1 | head -n $two | tail -1 | grep $letter >/dev/null ; set r2 $status ;
     if test $r1 -ne $r2 ; 
