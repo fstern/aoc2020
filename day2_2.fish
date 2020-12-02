f1330872bbd85c73fca47cc191497e316f5febb2
Falk Stern <falk@fourecks.de>
2020-12-02 23:43:20 +0100

    Fixed logic

--- day2_2.fish
+++ day2_2.fish
@@ -5,10 +5,12 @@ while read num letter password ;
     set one ( echo $num|cut -d- -f1 )
     set two ( echo $num|cut -d- -f2 )
     set letter ( echo $letter | sed s/:// )
-    if echo $password | fold -w 1 | head -n $one | tail -1 | grep $letter >/dev/null ;
-       or echo $password | fold -w 1 | head -n $two | tail -1 | grep $letter >/dev/null ; 
+    #echo $letter $one $two $password
+    echo $password | fold -w 1 | head -n $one | tail -1 | grep $letter >/dev/null ; set r1 $status ;
+    echo $password | fold -w 1 | head -n $two | tail -1 | grep $letter >/dev/null ; set r2 $status ;
+    if test $r1 -ne $r2 ; 
         set count ( math $count + 1 )
     end
-end < input_day2.txt
+end < input_day2.txt 
 
 echo $count 
